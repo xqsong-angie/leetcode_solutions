b62c8f7fa3f7ea801e87b42d12c00a8d2469267d
sxq <sxq@MacBook-Pro.local>
2026-05-29 17:39:29 -0500

    20250529

--- question_types/2_Linked List/707_Design Linked List/707_solutions.py
+++ question_types/2_Linked List/707_Design Linked List/707_solutions.py
@@ -44,7 +44,7 @@ class MyLinkedList:
                     if cur.next!=None:
                         cur=cur.next
                 if not cur:
-                    return
+                    return#这里没用，所以这个代码能accept，但不对
                 new_node.next=cur.next
                 cur.next=new_node    
 
@@ -67,6 +67,88 @@ class MyLinkedList:
                 cur.next=cur.next.next
 
 
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
+
+
+#20260528
+
+class ListNode:
+    def __init__(self,val): 
+        self.val=val
+        self.next=None
+
+class MyLinkedList:
+    def __init__(self):
+        self.head=None
+
+    def get(self, index: int) -> int:
+        if index<0:
+            return -1
+        else:
+            cur=self.head
+            for _ in range(index):
+                if cur.next:
+                    cur=cur.next
+                else:
+                    return -1
+            return cur.val if cur!=None else -1
+
+    def addAtHead(self, val: int) -> None:
+        new_head=ListNode(val)
+        new_head.next=self.head
+        self.head=new_head
+
+    def addAtTail(self, val: int) -> None:
+        new_node=ListNode(val)
+        if not self.head:
+            self.head=new_node
+        else:
+            cur=self.head
+            while cur.next!=None:
+                cur=cur.next
+            cur.next=new_node
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index==0:
+            self.addAtHead(val)
+        else:
+            cur=self.head
+            new_node=ListNode(val)
+            for _ in range(index-1):
+                if cur:
+                    cur = cur.next
+                else:
+                    return
+            if not cur:
+                return
+            temp=cur.next
+            new_node.next=temp
+            cur.next=new_node
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index==0 and not self.head:
+            return
+        elif index==0:
+            if self.head.next:
+                self.head=self.head.next
+            else:
+                self.head=None
+        else:
+            cur=self.head
+            for _ in range(index-1):
+                if cur.next:
+                    cur=cur.next
+                else:
+                    return
+            if cur.next:
+                cur.next=cur.next.next
 
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
