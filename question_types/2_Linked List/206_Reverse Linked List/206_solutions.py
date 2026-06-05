67db8a4de7e6fcd5045814d8c661c16fec7d3c0e
sxq <sxq@MacBook-Pro.local>
2026-06-04 19:47:40 -0500

    add new solution to 206

--- question_types/2_Linked List/206_Reverse Linked List/206_solutions.py
+++ question_types/2_Linked List/206_Reverse Linked List/206_solutions.py
@@ -37,4 +37,36 @@ class Solution:
         pre=None
         return self.reverse(pre,cur)
 
+#20260601
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head or not head.next:
+#             return head
+#         else:
+#             prev=head
+#             cur=head.next
+#             while cur:
+#                 next=cur.next
+#                 cur.next=prev
+#                 prev.next=None #会斩断数组，使prev连接不上之前的节点
+#                 prev=cur
+#                 cur=next
+#             return prev
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        cur = head
+        
+        while cur:
+            next_node = cur.next  # 1. 临时保存下一个节点，防止“迷路”
+            cur.next = prev       # 2. 核心：调转方向！让当前节点指向前一个
+            prev = cur            # 3. prev 前进一步
+            cur = next_node       # 4. cur 前进一步
+            
+        return prev
 
