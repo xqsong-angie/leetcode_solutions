0e2409a9c8bcc245c8000f6b9d0c32ee32632c9e
sxq <songxq200316@163.com>
2026-06-22 17:26:01 -0500

    20260621

--- question_types/1_Array/26_Remove Duplicates from Sorted Array/26_solutions.py
+++ question_types/1_Array/26_Remove Duplicates from Sorted Array/26_solutions.py
@@ -14,4 +14,21 @@ class Solution:
         k=i+1
         return k
 
-#20260620
+#20260621
+#错：
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j=k=0
+        n=len(nums)
+        cnt=0
+        for i in range(n):
+            if i>0 and nums[i]==nums[i-1]:
+                j=i
+                cnt+=1
+            elif nums[i]!=nums[i-1]: #i=0时，直接进入这里，变成nums[-1]，逻辑混乱
+                k=i-1
+                nums[j]+=nums[i]
+                j+=1
+                if j==k:
+                    continue
+        return len(nums) #这里把后面重复的也算上了
--- question_types/5_Stacks and Queues/150_Evaluate Reverse Polish Notation/150_solutions.py
+++ question_types/5_Stacks and Queues/150_Evaluate Reverse Polish Notation/150_solutions.py
@@ -24,3 +24,29 @@ class Solution:
                 elif tokens[i]=="/":
                     mystack.append(top1/top2)
         return int(mystack[-1])
+    
+#20260621
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack=[]
+        n=len(tokens)
+        for i in range(n):
+            if tokens[i]=="/":
+                num1=stack.pop()
+                num2=stack.pop()
+                stack.append(int(num2)/int(num1)) #"//" vs "/"
+            elif tokens[i]=="+":
+                num1=stack.pop()
+                num2=stack.pop()
+                stack.append(int(num2)+int(num1))
+            elif tokens[i]=="*":
+                num1=stack.pop()
+                num2=stack.pop()
+                stack.append(int(num2)*int(num1))
+            elif tokens[i]=="-": #不要忘了这里，分单独的"-"和“-11”这种连着数字的
+                num1=stack.pop()
+                num2=stack.pop()
+                stack.append(int(num2)-int(num1))
+            else:
+                stack.append(tokens[i])
+        return int(stack[-1])
